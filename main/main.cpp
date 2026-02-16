c7e04541b967d3413b3fee44254769f539656e97
Nischal <nischaga@metropolia.fi>
2026-02-16 15:35:03 +0200

    usb tested.

--- main/main.cpp
+++ main/main.cpp
@@ -7,9 +7,14 @@
 
 
 bool is_usb_connected(void) {
-    return usb_serial_jtag_is_connected();
+    return usb_serial_jtag_is_connected
+    ();
+}
 
-while (true) {
-    printf("usb connection status: %d\n", usb_serial_jtag_is_connected() ? 1 : 0);
+extern "C" void app_main(void) {
+  while (true) {
+    printf("usb connection status: %d\n",
+           usb_serial_jtag_is_connected() ? 1 : 0);
     vTaskDelay(1000);
+  }
 }
